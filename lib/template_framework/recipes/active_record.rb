168bb0eac1c12c62462ea532b0b6ce677b8af2ff
Aaron Cruz <aaron@aaroncruz.com>
2012-07-24 20:27:56 +0200

    Added mysql to ORM section

--- lib/template_framework/recipes/active_record.rb
+++ lib/template_framework/recipes/active_record.rb
@@ -1,16 +1,22 @@
 db_options = {
   'Option' => 'Database Options',
   '1' => 'Posgresql',
-  '2' => 'Sqlite3'
+  '2' => 'Mysql',
+  '3' => 'Sqlite3'
 }
 
 print_table db_options.to_a, :ident => 4
 selection = ask("\nOption: ", Thor::Shell::Color::BLUE)
 
-if selection == '1'
+case selection
+when '1'
   @username = ask("\nDatabase username?", Thor::Shell::Color::BLUE)
   gem 'pg'
   template File.expand_path("../../templates/active_record/config/database_postgres.yml.erb", __FILE__), 'config/database.yml'
+when '2'
+  @username = ask("\nDatabase username?", Thor::Shell::Color::BLUE)
+  gem 'mysql2'
+  template File.expand_path("../../templates/active_record/config/database_mysql.yml.erb", __FILE__), 'config/database.yml'
 else
   gem 'sqlite3'
   create_file 'config/database.yml', templater.load_template('config/database.yml', 'active_record')
